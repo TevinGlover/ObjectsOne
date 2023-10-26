a8c95c441049b767cb0f5f2b66ad40a8954c7426
Tevin <tevin@Zipcoders-MacBook-Pro-5.local>
2023-10-26 13:11:31 -0400

    getting close

--- object1/src/main/java/object1/Car.java
+++ object1/src/main/java/object1/Car.java
@@ -1,2 +1,49 @@
-package object1;public class Car {
+package object1;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private int mileage;
+
+    public Car(String make, String model, int year, int mileage) {
+        this.make = make;
+
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.make = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.year = mileage;
+    }
 }
+
