2561c113e90d6c39958238827d7ad9f9180832d9
Tevin <tevin@Zipcoders-MacBook-Pro-5.local>
2023-10-25 20:11:52 -0400

    it was hard

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,10 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
+        Person person1 = new Person("tevin",30,67);
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        logger.info(person1.getHeight() + "");
+    }
+
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,28 @@
+package object1;
+
+public class Person {
+    private String name;
+    private int age;
+    private int height;
+
+
+        // Constructor
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.height = height;
+        this.age = age;
+        }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getHeight(){
+        return height;
+    }
+    }
+
+
+
+
